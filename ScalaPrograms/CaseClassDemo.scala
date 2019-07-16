ae7c2a6c95a6c3022e50cad4a342e8022825aad3
Samru5 <48197286+Samru5@users.noreply.github.com>
2019-07-16 16:34:40 +0530

    add

--- ScalaPrograms/CaseClassDemo.scala
+++ ScalaPrograms/CaseClassDemo.scala
@@ -1,6 +1,7 @@
 
 
 //Program of case class
+//Eg-1
 object CaseClassDemo extends App {
 
   case class Employee(empId: Int, empName: String, empSalary: Float)
@@ -19,6 +20,31 @@ object CaseClassDemo extends App {
 
 
 }
+
+//Eg-2
+object demo extends App{
+
+  case class Employee(name:String,salary:Float)
+
+  val emp=Employee("Seema",44000)
+
+  println("Name is-"+emp.name)
+  println("Salary is-"+emp.salary)
+
+  val emp1=Employee("Jay",2000)
+
+  println("Name is-"+emp1.name)
+  println("Salary is-"+emp1.salary)
+
+  val emp2=emp.copy()                     //creating copy of emp and storing in emp2
+  println("Name -"+emp2.name)
+  println("Salary -"+emp2.salary)
+
+  val emp3=emp.copy(name="Tony")        //creating copy of emp and storing in emp3 only changing its name
+  println("Name -"+emp3.name)
+  println("Salary -"+emp3.salary)
+}
+
 
 
 
